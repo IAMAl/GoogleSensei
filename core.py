68aea42aa964d4101be31432b29937a0e396557b
IAMAl <rentsrule@gmail.com>
2021-01-01 19:20:57 +0900

    comment-out unnecessary print

--- core.py
+++ core.py
@@ -17,16 +17,16 @@ def scrape_serps(key,maxrank,df_api,api_counter,j,try_cnt,file_wt_en=False,file_
         cnt=1
         while(cnt<maxrank*10):
             if api_counter < 99:
-                print("[page_num]-->"+str(cnt))
+                #print("[page_num]-->"+str(cnt))
                 API_KEY = df_api["API_KEY"][j]
                 ENGINE_ID = df_api["API_ID"][j]
-                print (CYAN+"[api_cnt]-->"+GREEN+str(api_counter)+ENDC)
-                print ("[API_KEY]-->"+str(API_KEY))
-                print ("[API_NUM]-->"+str(j))
-                print ("[SUGGEST]-->"+key)
+                #print (CYAN+"[api_cnt]-->"+GREEN+str(api_counter)+ENDC)
+                #print ("[API_KEY]-->"+str(API_KEY))
+                #print ("[API_NUM]-->"+str(j))
+                #print ("[SUGGEST]-->"+key)
 
                 req_url = "https://www.googleapis.com/customsearch/v1?hl=ja&key="+API_KEY+"&cx="+ENGINE_ID+"&alt=json&q="+ phrase +"&start="+ str(cnt)
-                print("[req_url]-->"+str(req_url))
+                #print("[req_url]-->"+str(req_url))
                 headers = {"User-Agent": 'Mozilla /5.0 (iPhone; CPU iPhone OS 9_1 like Mac OS X) AppleWebKit/601.1.46 (KHTML, like Gecko) Version/9.0 Mobile/13B5110e Safari/601.1'}
 
                 req = urllib.request.Request(req_url)
@@ -34,12 +34,12 @@ def scrape_serps(key,maxrank,df_api,api_counter,j,try_cnt,file_wt_en=False,file_
                 dump = json.loads(res.read())
 
                 hit = dump["queries"]["request"][0]["totalResults"]
-                print(hit)
+                #print(hit)
                 for p in range(len(dump["items"])):
                     url_list.append(dump['items'][p]['link'])
                     title_list.append(dump['items'][p]['title'])
                     snippet_list.append(dump['items'][p]['snippet'].replace('\n',''))
-                print(GREEN+'[len_list]-->'+str(len(dump["items"]))+ENDC)
+                #print(GREEN+'[len_list]-->'+str(len(dump["items"]))+ENDC)
 
                 if int(hit) < 11:
                     cnt = 100
@@ -48,7 +48,7 @@ def scrape_serps(key,maxrank,df_api,api_counter,j,try_cnt,file_wt_en=False,file_
             else:
                 j = j + 1
                 api_counter = 0
-            print('_____________________________')
+            #print('_____________________________')
 
         if file_wt_en:
             with open(file_name+'.csv', 'a') as f:
